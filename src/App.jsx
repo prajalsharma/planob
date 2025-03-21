e2519816e51428dae63ddfae696f616c022578e8
Aditya Phasu <adityaphasu919@gmail.com>
2025-03-21 19:58:11 +0530

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+};
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import Logo from "../assets/planob.svg";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between pl-4 pr-8">
+      <nav className="bg-white flex items-center w-full justify-between">
+        <div>
+          <a href="#" className="font-bold text-2xl flex items-center text-primary-blue">
+            <img src={Logo} alt="" className="size-32" />
+            <span className="-translate-x-2">plano B</span>
+          </a>
+        </div>
+        <ul className="flex font-medium gap-8 text-xl">
+          <li>
+            <a href="#">Recommendations</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li></li>
+        </ul>
+        <button className="border-2 border-black rounded-full px-5 py-2">Subscribe</button>
+      </nav>
+    </header>
+  );
+};
+export default Header;
