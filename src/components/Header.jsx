bd416fab68a399f84e081a319261e4f1c1e7655f
Aditya Phasu <adityaphasu919@gmail.com>
2025-03-23 01:51:48 +0530

    Enhance Header component with responsive navigation and sidebar functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,128 @@
+import { motion, useCycle } from "motion/react";
 import Logo from "../assets/planob.svg";
+import LanguageSelector from "./LanguageSelector";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { MenuToggle } from "./MenuToggle";
+
+const links = [
+  {
+    name: "Recomendações",
+    url: "#",
+  },
+  {
+    name: "Serviços",
+    url: "#",
+  },
+  {
+    name: "Fale conosco",
+    url: "#",
+  },
+];
+
+const sidebarVariants = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 168px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(19px at 168px 40px)",
+    transition: {
+      delay: 0.2,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+const listVariants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
 
 const Header = () => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const largeScreen = useMediaQuery("(min-width: 768px)");
+
   return (
-    <header className="flex items-center justify-between pl-4 pr-8">
+    <header className="flex items-center justify-between py-2 pl-2 pr-5 md:pl-4 md:pr-7 md:py-0 fixed w-full z-10 bg-white">
+      {isOpen && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-black/80 z-2"
+          onClick={() => toggleOpen()}
+        />
+      )}
+
       <nav className="bg-white flex items-center w-full justify-between">
         <div>
           <a href="#" className="font-bold text-2xl flex items-center text-primary-blue">
-            <img src={Logo} alt="" className="size-32" />
+            <img src={Logo} alt="" className="size-17 md:size-26" />
             <p className="-translate-x-2">plano B</p>
           </a>
         </div>
-        <ul className="flex font-medium gap-8 text-xl">
-          <li>
-            <a href="#">Recomendações</a>
-          </li>
-          <li>
-            <a href="#">Serviços</a>
-          </li>
-          <li>
-            <a href="#">Fale conosco</a>
-          </li>
-          <li></li>
-        </ul>
-        <button className="border-2 border-black rounded-full px-5 py-2">Inscreva-se</button>
+        {largeScreen ? (
+          <ul className="font-medium gap-8 text-lg flex">
+            {links.map((link, index) => (
+              <li key={index}>
+                <a href={link.url} className="hover:text-primary-blue transition-colors">
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div></div>
+        )}
+
+        <motion.div className="flex items-center gap-4 relative">
+          <LanguageSelector />
+          {!largeScreen && (
+            <motion.div
+              initial={false}
+              animate={isOpen ? "open" : "closed"}
+              className="relative grid place-items-center z-50">
+              <motion.div
+                className="absolute -top-[30.5px] -right-5 w-50 h-screen bg-primary-blue"
+                variants={sidebarVariants}
+              />
+              <motion.ul variants={listVariants} className="absolute -left-34 w-full h-full top-11">
+                {links.map((link, index) => (
+                  <motion.li variants={itemVariants} key={index} className="py-4">
+                    <a href={link.url} className="text-white">
+                      {link.name}
+                    </a>
+                  </motion.li>
+                ))}
+              </motion.ul>
+              <MenuToggle toggle={() => toggleOpen()} />
+            </motion.div>
+          )}
+        </motion.div>
       </nav>
     </header>
   );
