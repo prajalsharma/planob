ab3be10ed396d9eea9ffcec18ebe18fe700e006d
DarthPrajal <prajalsharma1120@gmail.com>
2025-03-28 16:02:14 +0530

    Create Recommendations.jsx

---
+++ src/pages/Recommendations.jsx
@@ -0,0 +1,11 @@
+// Recommendations.jsx
+import React from 'react';
+
+export default function Recommendations() {
+  return (
+    <section>
+      <h1>Recommendations</h1>
+      {/* Your page content goes here */}
+    </section>
+  );
+}
