7b62e33fe154150b848aa7dacd2c8f4639a6dc4a
DarthPrajal <prajalsharma1120@gmail.com>
2025-04-10 14:14:48 +0530

    Update BlogDetail.jsx

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -125,7 +125,7 @@ const BlogDetail = () => {
         </div>
       ) : (
         <div className="flex flex-col gap-2 mt-5">
-          <h2 className="text-2xl font-semibold">Read More...</h2>
+          <h2 className="text-2xl font-semibold">Leia Mais...</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {blogs.map((blog, idx) => (
               <BlogCard key={idx} {...blog} />
